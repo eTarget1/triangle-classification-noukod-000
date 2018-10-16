4496caf0fad18574568a4e251228707f0df2df6d
clifford <clifford@noukod.com>
2018-10-16 15:51:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,10 @@
 class Triangle
   # write code here
   def initialize(side_1, side_2, side_3)
-    
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
+
   end
 end
